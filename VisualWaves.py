764bdcbaf487431c15476dc2d9e78a7ea6868f60
aliimakka <144072194+aliimakka@users.noreply.github.com>
2024-03-03 22:12:29 -0500

    c

--- VisualWaves.py
+++ VisualWaves.py
@@ -318,7 +318,7 @@ elif selected_theme == "Demographics":
             participants = 'sum(participants_race)',
             ).encode(
              theta=alt.Theta(f"total:Q", stack=True),
-             color=alt.Color("Race:N", legend=None),
+             color=alt.Color("Race:N", legend=None, scale=alt.Scale(scheme='dark2')),
              tooltip=['source', 'Year_Range','Race', 'participants:Q'],
          ).properties(
              width=10,
@@ -329,7 +329,7 @@ elif selected_theme == "Demographics":
          charts.append(pie)
 
      final_chart = alt.vconcat(*charts).resolve_scale(x='independent')
-     st.altair_chart(final_chart, use_container_width=True)
+     st.altair_chart(final_chart, use_container_width=True, )
 
 
 
