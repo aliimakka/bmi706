deeabcdc878f4a4589ea1d62eafcffd2bb01616e
aliimakka <144072194+aliimakka@users.noreply.github.com>
2024-03-04 01:15:26 -0500

    .

--- VisualWaves.py
+++ VisualWaves.py
@@ -345,11 +345,11 @@ elif selected_theme == "Demographics":
           color=alt.Color('Race:N', sort=alt.EncodingSortField('sum(participants)', order='descending')),
           tooltip=['source', 'Race:N', 'year', 'sum(participants)'],
           #).add_selection(race_source_selection
-         ).transform_filter(race_source_selection).properties(
+         ).transform_filter(race_source_selection).properties(width=100,
                 title=f'Demographics in trials sponsored by selected funding source from {selected_year[0]} to {selected_year[1]}'
            )
      chart2= alt.vconcat(final_chart,plot3)    
-     st.altair_chart(chart2)
+     st.altair_chart(chart2, use_container_width=True )
 
 
 
