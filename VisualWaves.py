f5902dcb9405354bed8afd41f8cd18b28cfcc72b
aliimakka <144072194+aliimakka@users.noreply.github.com>
2024-03-03 19:38:56 -0500

    committing

--- VisualWaves.py
+++ VisualWaves.py
@@ -312,7 +312,7 @@ elif selected_theme == "Demographics":
          ).encode(
          theta=alt.Theta("total:Q", stack=True),
          color=alt.Color("Race:N", legend=None),
-         tooltip=['source', 'Year_Range','Race', 'participants', 'percentage'],
+         tooltip=['source', 'Year_Range','Race', 'participants:Q', 'percentage:Q'],
      )
 
      pie = base.mark_arc(outerRadius=80)
