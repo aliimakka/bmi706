79148b9275626742647cc864b337cca246967ad0
aliimakka <144072194+aliimakka@users.noreply.github.com>
2024-03-04 01:21:52 -0500

    .

--- VisualWaves.py
+++ VisualWaves.py
@@ -345,7 +345,9 @@ elif selected_theme == "Demographics":
           color=alt.Color('Race:N', sort=alt.EncodingSortField('sum(participants)', order='descending')),
           tooltip=['source', 'Race:N', 'year', 'sum(participants)'],
           #).add_selection(race_source_selection
-         ).transform_filter(race_source_selection).properties(width=100,
+         ).transform_filter(race_source_selection).properties(
+              width=200,
+              width=200,
                 title=f'Demographics in trials sponsored by selected funding source from {selected_year[0]} to {selected_year[1]}'
            )
      chart2= alt.vconcat(plot3,final_chart)    
