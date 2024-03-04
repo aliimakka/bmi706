293610049aa1a91781160ccf256f74abb3951ba5
aliimakka <144072194+aliimakka@users.noreply.github.com>
2024-03-03 20:29:54 -0500

    commit

--- VisualWaves.py
+++ VisualWaves.py
@@ -297,7 +297,8 @@ elif selected_theme == "Demographics":
          df_race['Year_Range'] = df_race['year'].astype(str)
 
      df_race['NormalizedValueRace'] = (df_race.groupby(['Year_Range', 'source','Race', ])['participants_race'].transform(lambda x: (x / x.sum())*100 if x.sum() != 0 else np.nan))
-     df_race_non_zero = df_race[df_race[df_race['NormalizedValueRace'] != 0].notna()]
+     df_race_non_zero = df_race[df_race['NormalizedValueRace'].notna()]
+   
 
      st.write(df_race_non_zero.head())
 
